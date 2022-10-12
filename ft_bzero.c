26871d9459d2223ecedeabfd5fa2c5fa46d5034c
DanielAJL <daniellynch.dev@gmail.com>
2022-10-12 18:22:15 +0200

    add explanaton and testcase

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,10 +6,15 @@
 /*   By: dlynch <dlynch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 15:19:18 by dlynch            #+#    #+#             */
-/*   Updated: 2022/10/12 17:24:56 by dlynch           ###   ########.fr       */
+/*   Updated: 2022/10/12 18:16:49 by dlynch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+String ends at terminated null '\0'.
+FT_BZERO starts at the beginning of string, thus immediately terminating it.
+String should always be empty, unless 0 passed as n?
+*/
 #include "libft.h"
 
 void	*ft_bzero(void *s, size_t n)
@@ -33,8 +38,12 @@ int	main(void)
 	str[5] = 'X';
 	str[6] = 'Y';
 	printf("Before bzero => %s", str);
-	ft_bzero(str, 1);
-	bzero(str, 3);
+	ft_bzero(str, 0);
+	bzero(str, 0);
+	printf("\nAfter bzero => %s\n", str);
+	printf("Before bzero => %s", str);
+	ft_bzero(str, 5);
+	bzero(str, 5);
 	printf("\nAfter bzero => %s\n", str);
 	return (0);
 }
