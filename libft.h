b60add0f500dfe0a7a8cbe2604e957928671c775
DanielAJL <daniellynch.dev@gmail.com>
2022-10-12 17:53:04 +0200

    add #endif to fix error

--- libft.h
+++ libft.h
@@ -6,12 +6,13 @@
 /*   By: dlynch <dlynch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 14:38:47 by dlynch            #+#    #+#             */
-/*   Updated: 2022/10/10 16:26:03 by dlynch           ###   ########.fr       */
+/*   Updated: 2022/10/12 17:27:10 by dlynch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+
 # include <stdio.h>
 # include <unistd.h>
 # include <string.h>
@@ -21,3 +22,4 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 void	*ft_memset(void *str, int c, size_t len);
 void	*ft_bzero(void *s, size_t n);
+#endif
