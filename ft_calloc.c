32bcec0cabaffd95f261132f7bcc26fcbb8f2bad
DanielAJL <daniellynch.dev@gmail.com>
2022-10-19 16:07:11 +0200

    add calloc + fix for empty string test case

---
+++ ft_calloc.c
@@ -0,0 +1,37 @@
+#include "libft.h"
+
+/*
+The calloc() function contiguously allocates enough space for count objects
+that are size bytes of memory each and returns a pointer to the allocated
+memory.  The allocated memory is filled with bytes of value zero.
+*/
+
+// MALLOC EXAMPLE (NOT CALLOC) ptr = (int*) malloc(100 * sizeof(int));
+
+/*
+When changing '\0' to '0' it doesn't work on empty strings?
+Why is this the case? Man says fill with '0's, not terminating '0's.
+*/
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	char	*ptr;
+
+	ptr = malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	ft_memset(ptr, 0, (count * size));
+	return (ptr);
+}
+
+// int	main(void)
+// {
+// 	int     size = 8539;
+
+// 	void * d1 = ft_calloc(size, sizeof(int));
+
+// 	void * d2 = calloc(size, sizeof(int));
+// 	if (memcmp(d1, d2, size * sizeof(int)))
+// 		printf("failed");
+// 	return (0);
+// }
