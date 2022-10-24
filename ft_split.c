263e7a39b006c63905b9259edc8737579e52024a
DanielAJL <daniellynch.dev@gmail.com>
2022-10-24 17:21:40 +0200

    add word count in str

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dlynch <dlynch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 14:44:49 by dlynch            #+#    #+#             */
-/*   Updated: 2022/10/24 16:48:06 by dlynch           ###   ########.fr       */
+/*   Updated: 2022/10/24 17:21:25 by dlynch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,6 @@
 
 static	int	get_first_non_matching_position(char const *s1, int c);
 static	int	get_first_matching_position(char const *s1, int c);
-
-/*
-Returns an index position of the pointer passed where the pointer points to the
-first character found matching int c character. Can traverse through whole
-string if we adjust the pointer position each time.
-*/
-// static	char *get_first_matching_position(char const *s1, int c)
-// {
-// 	int		i;
-
-// 	i = 0;
-// 	while (s1[i])
-// 	{
-// 		if (s1[i] == c)
-// 			return ((char *) s1 + i);
-// 		i++;
-// 	}
-// 	return ((char *) s1);
-// }
-
-// /*
-// Returns an index position of the pointer passed where the pointer points to the
-// first character found NOT matching int c character.
-// */
-// static	char *get_first_non_matching_position(char const *s1, int c)
-// {
-// 	int		i;
-
-// 	i = 0;
-// 	while (s1[i])
-// 	{
-// 		if (s1[i] != c)
-// 			return ((char *) s1 + i);
-// 		i++;
-// 	}
-// 	return ((char *) s1);
-// }
-
 
 static	int	get_first_matching_position(char const *s1, int c)
 {
@@ -85,24 +47,50 @@ static	int	get_first_non_matching_position(char const *s1, int c)
 	return (i);
 }
 
+static	int	get_total_substring_count(char const *s1, int c)
+{
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 0;
+	while (s1[i])
+	{
+		if (s1[i] != c)
+		{
+			count ++;
+			while (s1[i] && s1[i] != c)
+				i++;
+		}
+		i++;
+	}
+	return (count);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	int	start;
 	int	end;
+	int	str_count;
 	char	*ptr;
 
 	start = get_first_non_matching_position(s, c);
 	end = get_first_matching_position(s + start, c); // also length from start
 	ptr = ft_substr(s, start, (size_t) end);
+	str_count = get_total_substring_count(s, c);
 
-	printf("start:	[%d]\n\n", start);
-	printf("end:	[%d]\n\n", end);
-	printf("ptr:	[%s]\n\n", ptr);
+
+
+	printf("start:		[%d]\n\n", start);
+	printf("end:		[%d]\n\n", end);
+	printf("ptr:		[%s]\n\n", ptr);
+	printf("str_count:	[%d]\n\n", str_count);
 }
+
 
 int	main(void)
 {
-	char	*string = "      split       this for   me  !       ";
+	char	*string = "      split       this for   me  ! ";
 	char	**expected = ((char*[6]){"split", "this", "for", "me", "!", NULL});
 	ft_split(string, ' ');
 	while (*expected)
