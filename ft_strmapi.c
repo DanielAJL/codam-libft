d08683cc32925e7f6e53ed9d8327587477ec7bb7
DanielAJL <daniellynch.dev@gmail.com>
2022-10-28 15:47:08 +0200

    add ft_strmapi.c

---
+++ ft_strmapi.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlynch <dlynch@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/28 15:34:27 by dlynch            #+#    #+#             */
+/*   Updated: 2022/10/28 15:46:52 by dlynch           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char	*str;
+	if (!s || !f)
+		return (NULL);
+	str = malloc(ft_strlen(s) * sizeof(char) + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
