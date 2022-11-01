ce3b5a050fa5b9c355b7b587747999e71108bc77
DanielAJL <daniellynch.dev@gmail.com>
2022-11-01 14:16:43 +0100

    passes all tests

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: dlynch <dlynch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 15:35:53 by dlynch            #+#    #+#             */
-/*   Updated: 2022/11/01 13:51:05 by dlynch           ###   ########.fr       */
+/*   Updated: 2022/11/01 14:09:01 by dlynch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char	*ft_strchr(const char *s, int c)
 	char	*ptr;
 	int		i;
 
+	if ((char) c == '\0')
+		return ((char *) s + ft_strlen(s));
 	ptr = (char *) s;
 	i = 0;
 	while (s[i])
@@ -29,18 +31,18 @@ char	*ft_strchr(const char *s, int c)
 	}
 	if (c == '\0')
 		return (ptr + i);
-	return (NULL);
+	return (0);
 }
 
 // int	main(void)
 // {
 // 	char	*ptr1;
 // 	char	*ptr2;
-// 	char s[] = "tripouille";
+// 	// char s[] = "tripouille";
 
-// 	ptr1 = ft_strchr(s, 't' + 256);
+// 	ptr1 = ft_strchr("teste", 1024);
 // 	// ptr1 = ft_strchr("fdsf\0fdsf", '\0');
-// 	ptr2 = strchr(s, 't' + 256);
+// 	ptr2 = strchr("teste", 1024);
 
 // 	puts(ptr1);
 // 	puts(ptr2);
